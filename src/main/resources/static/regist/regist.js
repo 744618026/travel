26f5ed870f38666a6286b643b38313fca20d3b7d
86158 <2289136157@qq.com>
2020-12-04 00:54:00 +0800

    -12-4-

---
+++ src/main/resources/static/regist/regist.js
@@ -0,0 +1,76 @@
+window.onload=function(){
+	let body = document.querySelector("body");
+	let width = document.documentElement.clientWidth;
+	let height =  document.documentElement.clientHeight;
+	body.style.width = width +'px';
+	body.style.height = height + "px";
+}
+window.onresize = function(){
+	let body = document.querySelector("body");
+	let width = document.documentElement.clientWidth;
+	let height =  document.documentElement.clientHeight;
+	body.style.width = width +'px';
+	body.style.height = height + "px";
+}
+$(document).ready(function () {
+	flgUsername = false;
+	$("#username").blur(function (){
+		flgUsername = blur("flgUsername","#username",".username_tip");
+		console.log(111)
+	})
+	$("#username").focus(function () {
+		focus(".username_tip");
+	})
+
+	flgPassword = false;
+	$("#password").blur(function (){
+		flgPassword = blur("flgPassword","#password",".password_tip");
+	})
+	$("#password").focus(function (){
+		focus(".password_tip");
+	})
+	flgConfirmPassword = false;
+	$("#confirmpassword").blur(function (){
+		flgConfirmPassword = blur("flgConfirmPassword","#confirmpassword",".confirmPassword_tip");
+	})
+	$("#confirmpassword").focus(function (){
+		focus(".confirmPassword_tip");
+	})
+	flgEmail = false;
+	$("#email").blur(function (){
+		flgEmail = blur("flgEmail","#email",".email_tip");
+	})
+	$("#email").focus(function (){
+		focus(".email_tip");
+	})
+	flgTelphone = false;
+	$("#telphone").blur(function (){
+		flgTelphone = blur("flgTelphone","#telphone",".telphone_tip");
+	})
+	$("#telphone").focus(function (){
+		focus(".telphone_tip");
+	})
+
+	$("#RegistNow").click(function (){
+		if(flgUsername&&flgPassword&&flgConfirmPassword&&flgEmail&&flgEmail&&flgTelphone){
+			$.ajax({
+				url:"",
+				
+			})
+		}
+	})
+});
+
+function blur(flg,id1,id2){
+	let e = $(id1).val();
+	if(e==""){
+		$(id2).css("display","block");
+		flg = false;
+	}else{
+		flg = true;
+	}
+	return flg;
+}
+function focus(id){
+	$(id).css("display","none");
+}
